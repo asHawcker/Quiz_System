ce1bdc771156cf91ffcc91d2ee40d308bd8987c2
saumadeepsardar <saumadeepsardar@gmail.com>
2024-12-05 10:36:12 +0530

    Added polymorphism to base class User

--- Admin.cpp
+++ Admin.cpp
@@ -1,11 +1,68 @@
+#include <fstream>
+#include <string>
+#include <limits>
+#include <sstream>
+#include <iostream>
 #include "Admin.hpp"
 #include "QuestionSet.hpp"
 #include "Question.hpp"
 #define pin 123456
 
+static string filename="AdminData.csv";
+
 Admin::Admin() : User(0)
 {
     qnos = 0;
+}
+
+void Admin::signup(int x)
+{
+    ofstream file;
+    file.open(filename, ios::app);
+    if (!file.is_open())
+    {
+        cerr << "Failed to open the file." << endl;
+        return;
+    }
+
+    while (x == 0)
+    {
+        cout << "Enter username: ";
+        getline(cin, username);
+        if (search(username))
+            cout << "Username already in use. Please try another name.\n";
+        else
+            break;
+    }
+
+    cout << "Enter your age: ";
+    cin >> age;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    cout << "Enter your email: ";
+    getline(cin, email);
+
+    cout << "Enter your password: ";
+    cin >> pass;
+
+    file << username << "," << age << "," << email << "," << pass << "," << type << "\n";
+    file.close();
+
+    cout << "Information saved successfully.\n"
+         << endl;
+}
+
+void Admin::signin()
+{
+    string searchName;
+    cout << "Enter your UserName: ";
+    cin >> searchName;
+
+    if (!search(searchName))
+    {
+        cout << "User with the name '" << searchName << "' not found.\n"
+             << endl;
+    }
 }
 
 void Admin::AdminInterface()
@@ -109,4 +166,38 @@ void Admin::Userlogin()
             AdminInterface();
         cout << endl;
     }
+}
+
+istream &operator>>(istream &in, User &u)
+{
+    cout << "Enter username: ";
+    getline(in, u.username);
+    if (u.search(u.username))
+    {
+        u.signin();
+    }
+    else
+    {
+        u.signup(1);
+    }
+    return in;
+}
+
+ostream &operator<<(ostream &out, const User &u)
+{
+    string acctype;
+
+    if (u.type == 0)
+        acctype = "Admin";
+    else if (u.type == 1)
+        acctype = "Player";
+    else
+        acctype = "Unknown";
+
+    out << "Username: " << u.username << endl
+        << "Age: " << u.age << endl
+        << "E-Mail: " << u.email << endl
+        << "Password: *********" << endl
+        << "Account type: " << acctype << endl;
+    return out;
 }
