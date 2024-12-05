044a9aae91089c59b4936013725b83a43ed0aa3f
ABISH MANGAL <abishmangal@gmail.com>
2024-12-06 03:22:23 +0530

    Separation of Interfaces

--- qSetDefs.cpp
+++ qSetDefs.cpp
@@ -9,3 +9,35 @@ int QuestionSet::show(){
     return 0;
 }
 
+int AddQuestion(QuestionSet& qSet, Question* q) {
+    if (qSet.set.size() >= qSet.size) throw("MaxLimitReached");    
+    qSet.set.push_back(q);
+    return 1;
+}
+
+int removeQuestion(QuestionSet& qSet, int qNumber) {
+    if (qSet.set.empty()) throw("EmptySet");
+    if (qNumber <= 0 || qNumber > static_cast<int>(qSet.set.size())){
+        throw("IndexOutOfRange");
+        return -1;
+    }   
+    delete qSet.set[qNumber - 1];
+    qSet.set.erase(qSet.set.begin() + qNumber - 1);
+    return 1;
+}
+Question* QuestionSet :: getQuestion(int x) const{
+        if (x<set.size()) return set[x];
+        else return set[x%set.size()];
+    }
+
+void QuestionSet :: saveToCSV() const {
+        ofstream file(filename);
+        if (!file.is_open()) {
+            throw runtime_error("Cannot open file for writing: " + filename);
+        }
+        for (const auto& q : set) {
+            string serialized = q->serialize();
+            file << serialized << endl;
+            
+        }
+    }
